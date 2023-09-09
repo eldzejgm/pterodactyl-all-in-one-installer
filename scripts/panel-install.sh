1216cc50536883da2474abc42df64cfe1d8c9644
EldzejGM <78844172+eldzejgm@users.noreply.github.com>
2023-09-09 11:04:39 +0200

    Update panel-install.sh

--- scripts/panel-install.sh
+++ scripts/panel-install.sh
@@ -1,19 +1,13 @@
 echo
-echo "1. Panel Installation"
-echo "2. Wings Installation"
-echo "4. Close / Cancel"
+echo "1. Pterodactyl Installer"
+echo "2. Close / Cancel"
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
     echo "Installing Pterodactyl Panel..."    
-    bash <(curl -s https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/v0.11.0/install-panel.sh)
+    bash <(curl -s https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer/master/install.sh)
 fi
 if [ $choice == "2" ]
-    then
-    echo "Installing Wings..."
-    bash <(curl -s https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/v0.11.0/install-wings.sh)
-fi
-if [ $choice == "4" ]
     then
     echo "Canceling..."
     exit 0
